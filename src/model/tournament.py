d2ffc5bc93424475236f1275f3d84a4315a487c0
Kevin Botsis <kevinbotsis.com>
2026-03-05 09:52:18 -0500

    Continued to work on tournament and added persistence folder

--- src/model/tournament.py
+++ src/model/tournament.py
@@ -1,4 +1,4 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 @dataclass
 class tournament:
@@ -7,14 +7,25 @@ class tournament:
     hostID: int
     hostName: str
     creation_date: tuple[int, int, int]
-    players: []
-
-    def addPlayers(newPlayers):
-        for new in newPlayers:
-            players.add[new]
+    players: list = field(default_factory=list)
+    
+    def addPlayers(self, newPlayers):
+         for new in newPlayers:
+            self.players.append(new)
             
     def setTournName(self, name):
         self.tournName = name
 
     def setTournID(self, id):
         self.tournID = id
+
+    def setHostID(self, id):
+        self.hostID = id
+    
+    def setHostName(self, name):
+        self.hostName = name
+
+    def setCreationDate(self, month, day, year):
+        self.creation_date = (month, day, year)
+
+    
